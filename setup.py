a7fb54097395b82cf93f06418641ec356ab018a1
Ethan Furman <ethan@stoneleaf.us>
2017-01-04 19:32:34 -0800

    import setuptools

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import setuptools; setuptools
 from distutils.core import setup
 
 long_desc = """\
@@ -238,7 +239,7 @@ make = []
 
 data = dict(
     name='pandaemonium',
-    version='0.7.0',
+    version='0.7.1a3',
     license='BSD License',
     description='Framework for writing daemons, with API similar to threading and multiprocessing.',
     long_description=long_desc,
