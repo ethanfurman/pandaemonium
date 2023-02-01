f6aab7d43b9a2224e811d2e7444252f77d0a3bfa
Ethan Furman <ethan@stoneleaf.us>
2023-01-31 20:52:17 -0800

    version 0.9.1

--- setup.py
+++ setup.py
@@ -224,7 +224,7 @@ make = []
 
 data = dict(
     name='pandaemonium',
-    version='0.9.0',
+    version='0.9.1',
     license='BSD License',
     description='Framework for writing daemons, with API similar to threading and multiprocessing.',
     long_description=long_desc,
