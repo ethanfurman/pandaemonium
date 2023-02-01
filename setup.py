b1e058ffadbab0522eb08fb4bb24acc50d459aec
Ethan Furman <ethan@stoneleaf.us>
2023-01-31 16:33:24 -0800

    ensure `timeout` is an integer; add more Python support

--- setup.py
+++ setup.py
@@ -259,6 +259,12 @@ data = dict(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Topic :: Software Development',
         ],
     )
